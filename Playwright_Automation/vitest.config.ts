07337a3a7b23120b73606e9cf4485cc5c90cd1f7
Mahadev-clanap <mahadev.gucchait@clanap.com>
2026-08-19 16:24:05 +0530

    update vi test and stryker

--- Playwright_Automation/vitest.config.ts
+++ Playwright_Automation/vitest.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vitest/config';
 
-const isStryker = Boolean(process.env.STRYKER_MUTATOR);
-
 export default defineConfig({
   test: {
     include: ['unit/**/*.test.ts'],
     environment: 'node',
-    reporters: isStryker ? ['dot'] : ['default', 'github-actions']
+    reporters: ['dot']
   }
 });
